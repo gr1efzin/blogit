f26639a1c7a489f84fc080da15eff040604b6ce4
gr1ef <subhajits1011@gmail.com>
2026-05-21 12:53:03 +0530

    added auth middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,40 @@
 import { Hono } from 'hono'
 import { PrismaClient, Prisma } from "./generated/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
-import { sign } from 'hono/jwt'
+import { sign, verify } from 'hono/jwt'
 
 const app = new Hono<{ 
   Bindings: { 
     DATABASE_URL : string,
     JWT_SECRET : string
-   }
+   },
+   Variables : {
+		userId: string
+	}
 }>();
+
+app.use('/api/v1/blog/*', async (c, next) => {
+  const header = c.req.header("Authorization");
+  
+  if(!header){
+    return c.json({
+      error : "Your authorization has expired\nPlease log/sign in again"
+    }, 403)
+  }
+  const token = header.split(" ")[1];
+  try{
+    const payload = await verify(token, c.env.JWT_SECRET, 'HS256');
+    if (!payload) {
+      c.status(401);
+      return c.json({ error: "Unauthorized" });
+    }
+    c.set('userId', payload.id as string);
+
+  }catch{
+    return c.json({ error: "Invalid or expired token" }, 403);
+  }
+  await next();
+})
 
 app.post('/api/v1/signup', async (c) => {
 
@@ -16,41 +42,41 @@ app.post('/api/v1/signup', async (c) => {
   accelerateUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
 
-try{
-  const body = await c.req.json();
-  const user = await prisma.user.create({
-    data:{
-      email : body.email,
-      name : body.name,
-      password : body.password
-    }
-  });
+  try{
+    const body = await c.req.json();
+    const user = await prisma.user.create({
+      data:{
+        email : body.email,
+        name : body.name,
+        password : body.password
+      }
+    });
 
-  const token = sign({id: user}, c.env.JWT_SECRET, 'HS256');
+  const token = await sign({id: user}, c.env.JWT_SECRET, 'HS256');
 
   return c.json({
     message : 'User Created',
     jwt: token
   }, 200)
+
 }catch(e: unknown){
   if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === "P2002") {
-    c.status(409);
     return c.json({ 
       error: "A user with this email already exists" 
-    });
+    }, 409);
   }
 
-  c.status(403);
   return c.json({
     error : "Error while signing up"
-  });
-}
+  }, 403);
+  }
 })
 
 app.post('/api/v1/signin', async (c) => {
 	const prisma = new PrismaClient({
 		accelerateUrl: c.env.DATABASE_URL	,
 	}).$extends(withAccelerate());
+
   try {
     const body = await c.req.json();
     const user = await prisma.user.findUnique({
@@ -60,15 +86,14 @@ app.post('/api/v1/signin', async (c) => {
     });
 
     if (!user) {
-      c.status(403);
-      return c.json({ error: "User not found" });
+      return c.json({ error: "User not found" }, 403);
     }
 
-    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET);
-    return c.json({ jwt });
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET, 'HS256');
+    return c.json({ jwt }, 200);
+
   } catch (e: unknown) {
-    c.status(403);
-    return c.json({ error: "Error while signing in" });
+    return c.json({ error: "Error while signing in" }, 403);
   }
 
 })
