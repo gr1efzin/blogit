452c7bfa5afa77326d40f870db23787dd4e3b8f5
gr1ef <subhajits1011@gmail.com>
2026-05-21 13:32:56 +0530

    added prisma context variable globally

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,15 +3,30 @@ import { PrismaClient, Prisma } from "./generated/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { sign, verify } from 'hono/jwt'
 
+const createPrisma = (env: { DATABASE_URL: string }) =>
+  new PrismaClient({ 
+    accelerateUrl: env.DATABASE_URL 
+  }).$extends(withAccelerate());
+
 const app = new Hono<{ 
   Bindings: { 
     DATABASE_URL : string,
     JWT_SECRET : string
    },
    Variables : {
-		userId: string
+		userId: string,
+    prisma: ReturnType<typeof createPrisma>
 	}
 }>();
+
+app.use('*', async (c, next) => {
+  const prisma = new PrismaClient({
+    accelerateUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate())
+  
+  c.set('prisma', prisma)
+  await next()
+})
 
 app.use('/api/v1/blog/*', async (c, next) => {
   const header = c.req.header("Authorization");
@@ -22,6 +37,7 @@ app.use('/api/v1/blog/*', async (c, next) => {
     }, 403)
   }
   const token = header.split(" ")[1];
+
   try{
     const payload = await verify(token, c.env.JWT_SECRET, 'HS256');
     if (!payload) {
@@ -38,9 +54,7 @@ app.use('/api/v1/blog/*', async (c, next) => {
 
 app.post('/api/v1/signup', async (c) => {
 
-  const prisma = new PrismaClient({
-  accelerateUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate());
+  const prisma = c.get('prisma');
 
   try{
     const body = await c.req.json();
@@ -52,30 +66,29 @@ app.post('/api/v1/signup', async (c) => {
       }
     });
 
-  const token = await sign({id: user}, c.env.JWT_SECRET, 'HS256');
+    const token = await sign({id: user.id}, c.env.JWT_SECRET, 'HS256');
 
-  return c.json({
-    message : 'User Created',
-    jwt: token
-  }, 200)
+    return c.json({
+      message : 'User Created',
+      jwt: token
+    }, 200)
 
-}catch(e: unknown){
-  if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === "P2002") {
-    return c.json({ 
-      error: "A user with this email already exists" 
-    }, 409);
-  }
+} catch(e: unknown){
+    if (e instanceof Prisma.PrismaClientKnownRequestError && e.code === "P2002") {
+      return c.json({ 
+        error: "A user with this email already exists" 
+      }, 409);
+    }
 
-  return c.json({
-    error : "Error while signing up"
-  }, 403);
+    return c.json({
+      error : "Error while signing up"
+    }, 403);
   }
 })
 
 app.post('/api/v1/signin', async (c) => {
-	const prisma = new PrismaClient({
-		accelerateUrl: c.env.DATABASE_URL	,
-	}).$extends(withAccelerate());
+  
+  const prisma = c.get('prisma');
 
   try {
     const body = await c.req.json();
