cdda31a3604b58387bd76a80890c9042f0a6e7a5
gr1ef <subhajits1011@gmail.com>
2026-05-24 13:35:09 +0530

    setting up the frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
+import { Signup } from './pages/Signup'
+import { Login } from './pages/Login'
+import { Blog } from './pages/Blog'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/blog/:id" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
